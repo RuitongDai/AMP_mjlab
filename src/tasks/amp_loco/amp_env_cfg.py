caeb27d49d13b3860ddedcd04d95ba030af3c460
Ruitong Dai <3192529076@qq.com>
2026-06-16 10:40:56 +0800

    “数据集切割，修改训练速度”

--- src/tasks/amp_loco/amp_env_cfg.py
+++ src/tasks/amp_loco/amp_env_cfg.py
@@ -196,9 +196,9 @@ def make_amp_env_cfg() -> ManagerBasedRlEnvCfg:
       heading_control_stiffness=0.5,
       debug_vis=True,
       ranges=UniformVelocityCommandCfg.Ranges(
-        lin_vel_x=(-1.0, 1.5),
-        lin_vel_y=(-1.0, 1.0),
-        ang_vel_z=(-1.5, 1.5),
+        lin_vel_x=(-0.3, 0.6),
+        lin_vel_y=(-0.2, 0.2),
+        ang_vel_z=(-0.9, 0.9),
         heading=(-math.pi / 2, math.pi / 2),
       ),
     )
